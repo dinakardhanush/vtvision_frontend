e0af1683f208ecacfb0c67b30336958c158593e7
Surya Prakash <suryaaprakashrj@gmail.com>
2023-02-25 01:43:29 +0530

    removed mode_modules

--- src/App.js
+++ src/App.js
@@ -2,20 +2,99 @@ import { BsFillShieldLockFill, BsTelephoneFill } from "react-icons/bs";
 import { CgSpinner } from "react-icons/cg";
 
 import OtpInput from "otp-input-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
-import { auth } from "./firebase.config";
+import { auth, db } from "./firebase.config";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import { toast, Toaster } from "react-hot-toast";
+// import "./App.css";
+import { uid } from "uid";
+import { onValue, ref } from "firebase/database";
+//import firebase from "firebase/app";
+import "firebase/database";
 
 const App = () => {
-
   const [otp, setOtp] = useState("");
   const [ph, setPh] = useState("");
   const [loading, setLoading] = useState(false);
   const [showOTP, setShowOTP] = useState(false);
   const [user, setUser] = useState(null);
+  const [vehicleNumber, setVehicleNumber] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [userList, setUserList] = useState([]);
+
+  const handleInputChange = (event) => {
+    setVehicleNumber(event.target.value);
+  };
+
+  useEffect(() => {
+    if (submitted) {
+      // const data = db
+      //   .ref(`users2/${vehicleNumber}`)
+      //   .orderByChild("vehicleNumber")
+      //   .equalTo(vehicleNumber);
+      onValue(ref(db, `/opencv`), (snapshot) => {
+        const users = snapshot.val();
+        const userList = [];
+        if (users !== null) {
+          //console.log(users.userId1);
+          users.forEach((element) => {
+            if (element.vehicleNumber === vehicleNumber) {
+              const user = {
+                vehicleNumber: element.vehicleNumber,
+                cctv: element.cctv,
+                date: element.date,
+              };
+              userList.push(user);
+            }
+
+            // for (let key in users) {
+            //   if (users[key].vehicleNumber === vehicleNumber) {
+            //     const user = {
+            //       id: key,
+            //       vehicleNumber: users[key].vehicleNumber,
+            //       cctv: users[key].cctv,
+            //     };
+            //     userList.push(user);
+            //   }
+            // }
+          });
+          setUserList(userList);
+        }
+      });
+
+      // console.log(data);
+    }
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+    console.log(vehicleNumber);
+    // const usersRef = db.ref("/users");
+
+    // usersRef.on("value", (snapshot) => {
+    //   const users = snapshot.val();
+    //   const userList = [];
+    // for (let key in users) {
+    //   const user = {
+    //     id: key,
+    //     vehicleNumber: users[key].vehicleNumber,
+    //     cctv: users[key].cctv,
+    //   };
+    //   userList.push(user);
+    // }
+    //   setUserList(userList);
+    // });
+
+    // const db = getDatabase(app);
+    // const userList = ref(db, "/users");
+    // onValue(userList, (snapshot) => {
+    //   const data = snapshot.val();
+    //   setUserList(...userList, data);
+    // });
+  };
 
   function onCaptchVerify() {
     if (!window.recaptchaVerifier) {
@@ -68,18 +147,50 @@ const App = () => {
         setLoading(false);
       });
   }
-    
-  return (
 
-             
-    <section className="bg-emerald-500 flex items-center justify-center h-screen">
+  return (
+    <section className="bg-emerald-500 flex justify-center h-screen">
       <div>
         <Toaster toastOptions={{ duration: 4000 }} />
         <div id="recaptcha-container"></div>
         {user ? (
-          <h2 className="text-center text-white font-medium text-2xl">
-             Mobile No: {`+91 `+ ph.slice(2,)}
-          </h2>
+          <div>
+            <h2 className="text-center font-medium text-2xl">
+              Mobile No: {`+91 ` + ph.slice(2)}
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={vehicleNumber}
+                onChange={handleInputChange}
+                placeholder="Enter Your Vehicle No:"
+              />
+              <button type="submit">Submit</button>
+            </form>
+            {submitted && (
+              <div>
+                <h1>Vehicle No: {vehicleNumber}</h1>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Vehicle Number</th>
+                      <th>CCTV Name</th>
+                      <th>Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {userList.map((user) => (
+                      <tr key={user.id}>
+                        <td>{user.vehicleNumber}</td>
+                        <td>{user.cctv}</td>
+                        <td>{user.date}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
         ) : (
           <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
             <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,22 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPFsaTSPBr2nuBCo5SbxL8-utemYCPdMY",
-  authDomain: "hackmoldemo.firebaseapp.com",
-  projectId: "hackmoldemo",
-  storageBucket: "hackmoldemo.appspot.com",
-  messagingSenderId: "548348965494",
-  appId: "1:548348965494:web:021bfbd9830d1018e2a557",
-  measurementId: "G-SLL4BBGGWZ"
+  apiKey: "AIzaSyD76F6ep9LQ--nm60kpLIsdIaX-8Ss7tKU",
+  authDomain: "hackmoldemo2.firebaseapp.com",
+  projectId: "hackmoldemo2",
+  storageBucket: "hackmoldemo2.appspot.com",
+  messagingSenderId: "942612640001",
+  appId: "1:942612640001:web:2329a152d23183132feb00",
+  measurementId: "G-HB065D2L29",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getDatabase(app);
